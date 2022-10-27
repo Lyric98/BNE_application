aa4d2384dda8fbf7c706c6b3735f12a2f30104d6
Lyric98 <yanranli@hsph.harvard.edu>
2022-10-27 09:25:51 -0400

    relu to .062

--- Rcode/10foldresult.R
+++ Rcode/10foldresult.R
@@ -3,3 +3,8 @@ rmse_mean_std <- read.table("test.txt",              # TXT data file indicated a
            header = TRUE,    # Whether to display the header (TRUE) or not (FALSE)
            sep = "",dec = " ")  
 View(rmse_mean_std)
+
+rmse_mean_cos <- read.table("cosine_rmse.txt",              # TXT data file indicated as string or full path to the file
+                            header = TRUE,    # Whether to display the header (TRUE) or not (FALSE)
+                            sep = "",dec = " ")  
+View(rmse_mean_cos)
