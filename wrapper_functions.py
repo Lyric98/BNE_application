2f79dd0bf4e2e9e109abf3d9cfdd87c1267981f2
Lyric98 <yanranli@hsph.harvard.edu>
2023-03-16 22:50:46 -0400

    overlay code

--- wrapper_functions.py
+++ wrapper_functions.py
@@ -1942,3 +1942,63 @@ def calc_prediction_std(y_pred, y):
   std = tf.sqrt(variance)
 
   return std
+
+
+# @title Visualization: posterior_heatmap_2d
+def posterior_heatmap_2d_tr(plot_data, X,
+                         X_monitor=None,
+                         cmap='inferno_r',
+                         norm=None, 
+                         norm_method="percentile",
+                         save_addr=''):
+    """Plots colored 2d heatmap using scatterplot.
+
+    Args:
+        plot_data: (np.ndarray) plot data whose color to visualize over
+            2D surface, shape (N, ).
+        X: (np.ndarray) locations of the plot data, shape (N, 2).
+        X_monitor: (np.ndarray or None) Locations to plot data points to.
+        cmap: (str) Name of color map.
+        norm: (BoundaryNorm or None) Norm values to adjust color map.
+            If None then a new norm will be created according to norm_method.
+        norm_method: (str) The name of method to compute norm values.
+            See util.visual.make_color_norm for detail.
+        save_addr: (str) Address to save image to.
+
+    Returns:
+        (matplotlib.colors.BoundaryNorm) A color norm object for color map
+            to be passed to a matplotlib.pyplot function.
+    """
+#     if save_addr:
+#         pathlib.Path(save_addr).parent.mkdir(parents=True, exist_ok=True)
+#         plt.ioff()
+
+#     if not norm:
+#         norm = make_color_norm(plot_data, method=norm_method)
+
+    # 2d color plot using scatter
+    plt.figure(figsize=(10, 8))
+    
+    plt.scatter(x=X[:, 0], y=X[:, 1],
+                s=3,
+                c=plot_data, cmap=cmap, norm=norm)
+    cbar = plt.colorbar()
+
+    plt.scatter(X_tr[:, 0] * X_scale[0] + X_centr[0], X_tr[:, 1] * X_scale[1] +
+                X_centr[1], c="black", s=10, alpha=0.5, marker="s")  # different shape
+    plt.scatter(X_te[:, 0] * X_scale[0] + X_centr[0], X_te[:, 1] * X_scale[1] + X_centr[1], c="blue",
+                s=abs(nll_bae_each)*50)
+                #cmap='inferno_r', norm=color_norm_nll_each)
+
+    # adjust plot window
+    plt.xlim((np.min(X[:, 0]), np.max(X[:, 0])))
+    plt.ylim((np.min(X[:, 1]), np.max(X[:, 1])))
+    
+    # if save_addr:
+    #     plt.savefig(save_addr, bbox_inches='tight')
+    #     plt.close()
+    #     plt.ion()
+    # else:
+    #     plt.show()
+
+    return norm
