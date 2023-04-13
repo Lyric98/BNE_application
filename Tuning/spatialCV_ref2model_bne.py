32a317b44869c0055a14b2bf0b5dc0b0a9aa67cf
Lyric98 <yanranli@hsph.harvard.edu>
2023-04-13 15:35:20 -0400

    before the BNE results

--- Tuning/spatialCV_ref2model_bne.py
+++ Tuning/spatialCV_ref2model_bne.py
@@ -152,8 +152,6 @@ def get_bne_result(data_dict, moment_mode, bne_config):
                                 moment_mode=moment_mode,
                                 **bne_config)
 
-  #data_dict[f'{model_name}_samples'] = joint_samples['y']
-  #return data_dict
   return joint_samples
 
 # @title Simulation: get_bma_result
@@ -226,17 +224,13 @@ rmse_lr = []
 rmse_gam = []
 rmse_bma = []
 rmse_bae = []
-rmse_bma2 = []
 rmse_bma_mean = []
 
 
 nll_lr, nll_gam, nll_bma_mean, nll_bma, nll_bae = [], [], [], [], []
-nll_bma2 = []
-# initialize a dataframe to store lon, lat and raw error
-error_df = pd.DataFrame(columns=['lon', 'lat', 'raw_error'])
 
 coverage_lr, coverage_gam, coverage_bma_mean, coverage_bma, coverage_bae = 0, 0, 0, 0, 0
-coverage_bma2 = 0
+
 #from rpy2.robjects import pandas2ri
 # import R's "base" package
 base = importr('base')
@@ -257,7 +251,6 @@ mgcv = importr('mgcv')
 stats = importr('stats')
 ciTools = importr('ciTools')
 
-rmse_bma_mean, rmse_bma2, rmse_bae = [], [], []
 for fold_id in range(1,7):
     print(fold_id)
     X_tr, X_te = X_train1[spcv_id!=fold_id], X_train1[spcv_id==fold_id]
@@ -315,11 +308,9 @@ for fold_id in range(1,7):
     print('BMA-mean:', flush=True)
     data_dict, bma_mean_joint_samples = get_bma_result(
         data_dicts, bma_config=bma_config)
-    y_pred_bma_mean = np.mean(np.nan_to_num(
-        bma_mean_joint_samples['y']), axis=0)
+    y_pred_bma_mean = np.mean(np.nan_to_num(bma_mean_joint_samples['y']), axis=0)
     pred_std = calc_prediction_std(y_pred_bma_mean, Y_te)
-    bma_mean_pi = np.array(
-        [(y_pred_bma_mean - 1.96*pred_std).numpy(), (y_pred_bma_mean + 1.96*pred_std).numpy()])
+    bma_mean_pi = np.array([(y_pred_bma_mean - 1.96*pred_std).numpy(), (y_pred_bma_mean + 1.96*pred_std).numpy()])
     print(y_pred_bma_mean)
 
     # BMA.
@@ -327,12 +318,11 @@ for fold_id in range(1,7):
     bma_var_config['mcmc_initialize_from_map'] = bma_config['mcmc_initialize_from_map']
     print('BMA var config:', flush=True)
     print(bma_var_config)
-    bma_joint_samples = get_bne_result(data_dict, moment_mode='none',
-                                       bne_config=bma_var_config)
+    bma_joint_samples = get_bne_result(data_dict, moment_mode='none', bne_config=bma_var_config)
     y_pred_bma = np.mean(np.nan_to_num(bma_joint_samples['y']), axis=0)
     print(y_pred_bma)
     pred_std = calc_prediction_std(y_pred_bma, Y_te)
-    bma_pi2 = np.array([(y_pred_bma - 1.96*pred_std).numpy(),
+    bma_pi = np.array([(y_pred_bma - 1.96*pred_std).numpy(),
                        (y_pred_bma + 1.96*pred_std).numpy()])
 
     # BAE.
@@ -349,7 +339,7 @@ for fold_id in range(1,7):
     # save the rmse & nll for each fold
     rmse_bma_mean.append(rmse(Y_te, y_pred_bma_mean))
     nll_bma_mean.append(nll(Y_te, y_pred_bma_mean))
-    rmse_bma2.append(rmse(Y_te, y_pred_bma))
+    rmse_bma.append(rmse(Y_te, y_pred_bma))
     nll_bma.append(nll(Y_te, y_pred_bma))
     rmse_bae.append(rmse(Y_te, y_pred_bae))
     nll_bae.append(nll(Y_te, y_pred_bae))
@@ -357,13 +347,13 @@ for fold_id in range(1,7):
     # save the coverage for each fold
     coverage_bma_mean += np.sum([(Y_te[i] > bma_mean_pi[0][i])
                                 & (Y_te[i] < bma_mean_pi[1][i]) for i in range(len(Y_te))])
-    coverage_bma += np.sum([(Y_te[i] > bma_pi2[0][i]) &
-                           (Y_te[i] < bma_pi2[1][i]) for i in range(len(Y_te))])
+    coverage_bma += np.sum([(Y_te[i] > bma_pi[0][i]) &
+                           (Y_te[i] < bma_pi[1][i]) for i in range(len(Y_te))])
     coverage_bae += np.sum([(Y_te[i] > bae_pi[0][i]) &
                            (Y_te[i] < bae_pi[1][i]) for i in range(len(Y_te))])
 
     print("rmse:", flush=True)
-    print(rmse_bma_mean, rmse_bma2, rmse_bae)
+    print(rmse_bma_mean, rmse_bma, rmse_bae)
     print("nll:", flush=True)
     print(nll_bma_mean, nll_bma, nll_bae)
 
@@ -385,7 +375,7 @@ print("Coverage BMA: ", coverage_bma/len(Y_train))
 with open('spcv0412.txt', 'a') as f:
     f.write('\n')
     f.write(''.join(str(bne_gp_lengthscale)+ " "+str(bne_gp_l2_regularizer) + " "+ 
-                    str(np.mean(rmse_bma2)) + " "+ str(np.median(rmse_bma2))+ " "+str(np.std(rmse_bma2))+ " "+str(coverage_bma/len(Y_train))+ " "+
+                    str(np.mean(rmse_bma)) + " "+ str(np.median(rmse_bma))+ " "+str(np.std(rmse_bma))+ " "+str(coverage_bma/len(Y_train))+ " "+
                     str(np.mean(nll_bma)) + " "+ str(np.median(nll_bma))+ " "+ 
                     str(np.mean(rmse_bae)) + " "+ str(np.median(rmse_bae))+ " "+str(np.std(rmse_bae))+ " "+str(coverage_bae/len(Y_train))+ " "+
                     str(np.mean(nll_bae)) + " "+ str(np.median(nll_bae))))
